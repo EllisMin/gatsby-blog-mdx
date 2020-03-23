f2240aff0aefe2f5485eeb3a3bbf9c9b68c4a571
Ellis Min <dbmin@hotmail.com>
2020-03-23 06:26:40 -0500

    [REFACTOR]: refactored social media icons in header into component

---
+++ src/components/header-icon/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react"
+import PropTypes from "prop-types"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const HeaderIcon = ({ accountInfo, mediaName, preHref, icon }) => {
+  const [err, setErr] = useState(false)
+  const accountName = accountInfo.accountName
+    ? accountInfo.accountName
+    : accountInfo.emailAddress
+
+  useEffect(() => {
+    if (!accountName) {
+      setErr(true)
+      console.log(
+        "HeaderIcon: accountInfo must contain either accountName or emailAddress"
+      )
+    }
+  }, [err])
+
+  return (
+    <>
+      {accountName && accountName && accountInfo.showHeaderIcon && !err ? (
+        <a
+          href={`${preHref}${accountName}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            className={`icon-${mediaName} icon-fa`}
+            icon={icon}
+          />
+        </a>
+      ) : null}
+    </>
+  )
+}
+
+HeaderIcon.propTypes = {
+  accountInfo: PropTypes.object.isRequired,
+  mediaName: PropTypes.string,
+  preHref: PropTypes.string.isRequired,
+  icon: PropTypes.object.isRequired,
+}
+
+export default HeaderIcon
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import HeaderIcon from "../header-icon"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import {
   faGithub,
@@ -32,83 +32,54 @@ const Header = ({ siteTitle, showTitle }) => {
           {showTitle && <Link to="/">{`${siteTitle}`}</Link>}
         </h1>
         <StyledMediaIcons>
-          {config.socialMediaLinks.email.emailAddress &&
-            config.socialMediaLinks.email.emailAddress !== "" &&
-            config.socialMediaLinks.email.showHeaderIcon && (
-              <a
-                href={`mailto:${config.socialMediaLinks.email.emailAddress}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <StyledFA className="icon-email" icon={faEnvelope} />
-              </a>
-            )}
-          {config.socialMediaLinks.github.accountName &&
-            config.socialMediaLinks.github.accountName !== "" &&
-            config.socialMediaLinks.github.showHeaderIcon && (
-              <a
-                href={`https://github.com/${config.socialMediaLinks.github.accountName}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <StyledFA className="icon-github" icon={faGithub} />
-              </a>
-            )}
-          {config.socialMediaLinks.facebook.accountName &&
-            config.socialMediaLinks.facebook.accountName !== "" &&
-            config.socialMediaLinks.facebook.showHeaderIcon && (
-              <a
-                href={`https://facebook.com/${config.socialMediaLinks.facebook.accountName}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <StyledFA className="icon-facebook" icon={faFacebook} />
-              </a>
-            )}
-          {config.socialMediaLinks.instagram.accountName &&
-            config.socialMediaLinks.instagram.accountName !== "" &&
-            config.socialMediaLinks.instagram.showHeaderIcon && (
-              <a
-                href={`https://instagram.com/${config.socialMediaLinks.instagram.accountName}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <StyledFA className="icon-instagram" icon={faInstagram} />
-              </a>
-            )}
-          {config.socialMediaLinks.twitter.accountName &&
-            config.socialMediaLinks.twitter.accountName !== "" &&
-            config.socialMediaLinks.twitter.showHeaderIcon && (
-              <a
-                href={`https://twitter.com/${config.socialMediaLinks.twitter.accountName}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <StyledFA className="icon-twitter" icon={faTwitter} />
-              </a>
-            )}
-          {config.socialMediaLinks.linkedIn.accountName &&
-            config.socialMediaLinks.linkedIn.accountName !== "" &&
-            config.socialMediaLinks.linkedIn.showHeaderIcon && (
-              <a
-                href={`https://linkedin.com/${config.socialMediaLinks.linkedIn.accountName}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <StyledFA className="icon-linkedin" icon={faLinkedin} />
-              </a>
-            )}
-          {config.socialMediaLinks.medium.accountName &&
-            config.socialMediaLinks.medium.accountName !== "" &&
-            config.socialMediaLinks.medium.showHeaderIcon && (
-              <a
-                href={`https://medium.com/@${config.socialMediaLinks.medium.accountName}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <StyledFA className="icon-medium" icon={faMedium} />
-              </a>
-            )}
+          <HeaderIcon
+            accountInfo={config.socialMediaLinks.email}
+            mediaName={"email"}
+            preHref={"mailto:"}
+            icon={faEnvelope}
+          />
+
+          <HeaderIcon
+            accountInfo={config.socialMediaLinks.github}
+            mediaName={"github"}
+            preHref={"https://github.com/"}
+            icon={faGithub}
+          />
+
+          <HeaderIcon
+            accountInfo={config.socialMediaLinks.facebook}
+            mediaName={"facebook"}
+            preHref={"https://facebook.com/"}
+            icon={faFacebook}
+          />
+
+          <HeaderIcon
+            accountInfo={config.socialMediaLinks.instagram}
+            mediaName={"instagram"}
+            preHref={"https://instagram.com/"}
+            icon={faInstagram}
+          />
+
+          <HeaderIcon
+            accountInfo={config.socialMediaLinks.twitter}
+            mediaName={"twitter"}
+            preHref={"https://twitter.com/"}
+            icon={faTwitter}
+          />
+
+          <HeaderIcon
+            accountInfo={config.socialMediaLinks.linkedIn}
+            mediaName={"linkedin"}
+            preHref={"https://linkedin.com/"}
+            icon={faLinkedin}
+          />
+
+          <HeaderIcon
+            accountInfo={config.socialMediaLinks.medium}
+            mediaName={"medium"}
+            preHref={"https://medium.com/@"}
+            icon={faMedium}
+          />
         </StyledMediaIcons>
       </StyledMainHeaderInner>
     </StyledMainHeader>
@@ -141,7 +112,6 @@ const StyledMainHeaderInner = styled.div`
   padding: 0.6rem;
 `
 
-const StyledFA = styled(FontAwesomeIcon)``
 const StyledMediaIcons = styled.div`
   display: flex;
   justify-content: flex-end;
